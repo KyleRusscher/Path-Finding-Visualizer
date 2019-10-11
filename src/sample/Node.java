d46d802a966dfa9dc5feba4d9d8be5fdcc8018e6
Maxgagnon3 <44710978+Maxgagnon3@users.noreply.github.com>
2019-10-11 16:21:30 -0400

    node class

---
+++ src/sample/Node.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Node {
+    boolean isWall;
+    boolean isStart;
+    boolean isEnd;
+    boolean isVisited;
+    boolean isPath;
+    public Node() {
+        isWall = false;
+        isStart = false;
+        isEnd = false;
+        isVisited = false;
+        isPath = false;
+
+    }
+}
